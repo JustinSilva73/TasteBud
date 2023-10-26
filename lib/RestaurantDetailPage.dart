757b3ad1ebbc91e74c53b534046a8cc772004fa4
Francis Lapointe <flapointejr@gmail.com>
2023-10-25 23:03:44 -0400

    Updated Resturant Details Page

--- lib/RestaurantDetailPage.dart
+++ lib/RestaurantDetailPage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'MainPage.dart';  // Replace with the path to the file where the Restaurant class is defined.
 
@@ -13,30 +12,109 @@ class RestaurantDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(restaurant.name),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 16.0),
-            Text(
-              restaurant.name,
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+            Container(
+              width: double.infinity,
+              height: 200, // You can adjust the height as needed
+              child: Image.asset(
+                'assets/restaurant_image.jpg', // Replace with the path to your restaurant image
+                fit: BoxFit.cover,
               ),
             ),
-            SizedBox(height: 8.0),
-            Text(
-              restaurant.address,
-              style: TextStyle(
-                fontSize: 16.0,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    restaurant.name,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    restaurant.address,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Price Range:',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    '\$ - \$\$\$', // Replace with the actual price range
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Distance from You:',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    '5 miles', // Replace with the actual distance
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Hours of Operation:',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  TextField(
+                    readOnly: true, // Prevents user input
+                    controller: TextEditingController(text: 'Your hours of operation here'),
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Cuisine:',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  TextField(
+                    readOnly: true, // Prevents user input
+                    controller: TextEditingController(text: 'Your cuisine information here'),
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  // Add more details or widgets as needed
+                ],
               ),
             ),
-            // Add more details or widgets as needed
           ],
         ),
       ),
     );
   }
 }
+
+
