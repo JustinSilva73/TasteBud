e5124a2dc46055eda1770c3bba4d429657c7135c
Francis Lapointe <flapointejr@gmail.com>
2024-03-30 15:00:05 -0400

    Added Sliding Animation To Log In and Survey when going to Main Page Successfully

--- Frontend/lib/LogInPage.dart
+++ Frontend/lib/LogInPage.dart
@@ -300,7 +300,20 @@ class _LoginPageState extends State<LoginPage> {
                           if (loginSuccess) {
                             Navigator.pushReplacement(
                               context,
-                              MaterialPageRoute(builder: (context) => const MainPage()),
+                              PageRouteBuilder(
+                                pageBuilder: (context, animation, secondaryAnimation) => const MainPage(),
+                                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                  const begin = Offset(1.0, 0.0);
+                                  const end = Offset.zero;
+                                  const curve = Curves.easeInOut;
+                                  var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                                  var offsetAnimation = animation.drive(tween);
+                                  return SlideTransition(
+                                    position: offsetAnimation,
+                                    child: child,
+                                  );
+                                },
+                              ),
                             );
                           } else {
                             ScaffoldMessenger.of(context).showSnackBar(
